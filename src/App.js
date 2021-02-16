a9a06cfdd8b562bdd2d1aa93c4e5324798abef65
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-16 17:49:14 +0530

    remove anchor tag

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a>Learn React</a>
+        <a></a>
       </header>
     </div>
   );
